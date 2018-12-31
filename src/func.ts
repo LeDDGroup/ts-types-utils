060e3fa615e8e3501bf88fbf83b23d213f3ccd15
Daniel Perez Alvarez <danielpza@protonmail.com>
2018-12-31 12:28:31 -0500

    chore: make `ArgsType` use `Parameters`

--- src/func.ts
+++ src/func.ts
@@ -1,5 +1,3 @@
 export type Func<P extends any[], R> = (...args: P) => R;
 
-export type ArgsType<F extends Func<any, any>> = F extends Func<infer P, any>
-  ? P
-  : never;
+export type ArgsType<F extends Func<any, any>> = Parameters<F>;
